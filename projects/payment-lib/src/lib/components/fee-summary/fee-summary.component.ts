18eeac4e73b03f6ea410c655e7030f61e0fdafaf
subrotomukherjee <subroto.mukherjee@HMCTS.NET>
2019-10-10 15:34:17 +0100

    PAY-1911, PAY-1918, PAY-1881

--- projects/payment-lib/src/lib/components/fee-summary/fee-summary.component.ts
+++ projects/payment-lib/src/lib/components/fee-summary/fee-summary.component.ts
@@ -40,8 +40,6 @@ export class FeeSummaryComponent implements OnInit {
 
   ngOnInit() {
     this.viewStatus = 'main';
-    // this.paymentGroupRef = '2018-15310089885';
-    //this.paymentGroupRef = '2019-15496299273';
     this.getPaymentGroup();
   }
 
