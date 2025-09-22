3cfae14063f98d5572f1feb4409f3d1add555aee
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2025-09-22 19:15:40 +0100

    CME-779: suppress standalone and control flow errors on sonarqube

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,28 @@
-// .eslintrc.js example
+// .eslintrc.js
 module.exports = {
   "env": {
-      "browser": true,
-      "es2021": true
+    "browser": true,
+    "es2021": true,
+    "node": true
   },
-  "extends": "eslint:recommended",
+  "extends": [
+    "eslint:recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
   "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module"
+    "ecmaVersion": "latest",
+    "sourceType": "module"
   },
+  "plugins": ["@typescript-eslint"],
+  "rules": {
+    "@typescript-eslint/no-require-imports": "off"
+  },
+  "overrides": [
+    {
+      "files": ["src/assets/**/*.js"],
+      "rules": {
+        "@typescript-eslint/no-unused-expressions": "off"
+      }
+    }
+  ]
 }
-
